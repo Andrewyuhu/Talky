2b525b8e06f3ce4615e073c66faf86e2fe98cc4a
Andrew Hu <andrewyuhu02@gmail.com>
2025-04-04 17:42:07 -0700

    added better error handling for readJSON

--- backend/cmd/api/helper.go
+++ backend/cmd/api/helper.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"maps"
 	"net/http"
@@ -20,7 +23,33 @@ func (app *application) readJSON(r *http.Request, dst any) error {
 	err := js.Decode(dst)
 
 	if err != nil {
-		return err
+
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		// For anything else, return the error message as-is.
+		default:
+			return err
+		}
 	}
 
 	return nil
