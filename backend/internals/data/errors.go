36ec03a0f595f3e837ca6be0d7ec10a0421d70b4
Andrew Hu <andrewyuhu02@gmail.com>
2025-04-07 22:49:39 -0700

    adding eslint and fixed errors

--- backend/internals/data/errors.go
+++ backend/internals/data/errors.go
@@ -3,8 +3,8 @@ package data
 import "errors"
 
 var (
-	ErrDuplicateUsername  = errors.New("username already exists")
-	ErrRecordNotFound     = errors.New("record not found")
-	ErrDuplicateEmail     = errors.New("email already exists")
-	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrDuplicateUsername  = errors.New("USERNAME_EXISTS")
+	ErrRecordNotFound     = errors.New("RECORD_NOT_FOUND")
+	ErrDuplicateEmail     = errors.New("EMAIL_EXISTS")
+	ErrInvalidCredentials = errors.New("INVALID_CREDENTIALS")
 )
