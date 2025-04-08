abb7dfd3b7b5309e9b854467492901753d599930
Andrew Hu <andrewyuhu02@gmail.com>
2025-04-08 00:12:16 -0700

    added CORs processing

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"github.com/rs/cors"
 )
 
 func (app *application) router() http.Handler {
@@ -20,5 +21,12 @@ func (app *application) router() http.Handler {
 
 	router.Handler(http.MethodGet, "/v1/protected", protected.Then(http.HandlerFunc(app.healthcheckHandler)))
 
-	return router
+	corsRouter := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"}, // Your frontend's origin
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+	}).Handler(router)
+
+	return corsRouter
 }
